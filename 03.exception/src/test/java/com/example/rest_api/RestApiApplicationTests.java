0eb500d729435f3eb81d4b1f423bee973fe5c1e9
Hee Namkung <bnamkung@gmail.com>
2025-05-14 23:37:19 +0900

    Add lecture 3

---
+++ 03.exception/src/test/java/com/example/rest_api/RestApiApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example.rest_api;
+
+import com.example.rest_api.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	void contextLoads() throws JsonProcessingException {
+		var user = new UserRequest();
+		user.setUserName("Hee");
+		user.setUserAge(32);
+		user.setEmail("abc@gmail.com");
+		user.setIsKorean(true);
+
+		var json = objectMapper.writeValueAsString(user);
+		System.out.println(json);
+
+		var dto = objectMapper.readValue(json, UserRequest.class);
+		System.out.println(dto);
+
+	}
+
+}
